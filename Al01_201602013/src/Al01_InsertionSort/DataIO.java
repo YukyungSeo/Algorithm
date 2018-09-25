a73c95ce982eaecfba5cbb8842668bfd2affb515
YukungSeo <syk332@naver.com>
2018-09-25 11:53:52 +0900

    make bubblesort

---
+++ Al01_201602013/src/Al01_InsertionSort/DataIO.java
@@ -0,0 +1,51 @@
+package Al01_InsertionSort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+
+class DataIO {
+
+	// public method
+	public void readDataSet(String fileName, DoublyLinkedList<Node> dl) {
+		try {
+			File file = new File("C:\\Users\\SeoYukyung\\eclipse-workspace\\Algorithm\\Al01_201602013", fileName);
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				dl.append(new Node(Integer.parseInt(line)));
+				// System.out.println(line);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	public void writeDataSet(String fileName, Iterator<Node> itr) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+
+			while (itr.hasNext()) {
+				out.write(itr.next().toString());
+				out.newLine();
+			}
+
+			out.close();
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+
+	}
+}
+
---
+++ Al01_201602013/src/Al01_InsertionSort/Node.java
@@ -0,0 +1,51 @@
+package Al01_InsertionSort;
+
+class Node {
+	private Node preNode;
+	private Node nextNode;
+	private int element;
+
+	// construct
+	public Node(int data) {
+		this.preNode = null;
+		this.nextNode = null;
+		this.element = data;
+	}
+
+	public Node(int data, Node preNode, Node nextNode) {
+		this.preNode = preNode;
+		this.nextNode = nextNode;
+		this.element = data;
+	}
+
+	// getter & setter
+	public Node getPreNode() {
+		return preNode;
+	}
+
+	public void setPreNode(Node preNode) {
+		this.preNode = preNode;
+	}
+
+	public Node getNextNode() {
+		return nextNode;
+	}
+
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public int getElement() {
+		return element;
+	}
+
+	public void setElement(int element) {
+		this.element = element;
+	}
+
+	// public method
+	@Override
+	public String toString() {
+		return this.element + "";
+	}
+}
