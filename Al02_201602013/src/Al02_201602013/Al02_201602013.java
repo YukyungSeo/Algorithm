1745217eb617370f316c87f0a6b32fa0dcb8417a
YukungSeo <syk332@naver.com>
2018-10-08 00:00:59 +0900

    made quickSort

--- Al02_201602013/src/Al02_201602013/Al02_201602013.java
+++ Al02_201602013/src/Al02_201602013/Al02_201602013.java
@@ -8,19 +8,22 @@ public class Al02_201602013 {
 		mergeSort(dataio, "test_100.txt");
 		mergeSort(dataio, "test_1000.txt");
 
+		QuickSort(dataio, "test_10.txt");
+		QuickSort(dataio, "test_100.txt");
+		QuickSort(dataio, "test_1000.txt");
 	}
 
 	private static void mergeSort(DataIO io, String fileName) {
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		io.readDataSet(fileName, al);
-		al.sort_merSort();
-		io.writeDataSet("Merge_" + fileName, al.arrIterator());
+		ArrayList<Integer> mr = new ArrayList<Integer>();
+		io.readDataSet(fileName, mr);
+		mr.sort_mergeSort();
+		io.writeDataSet("Merge_" + fileName, mr.arrIterator());
 	}
 
-//	private static void QuickSort(DataIO io, String fileName) {
-//		DoublyLinkedList<Node> dll = new DoublyLinkedList<Node>();
-//		io.readDataSet(fileName, dll);
-//		dll.sort_bubbleSort();
-//		io.writeDataSet("Bubble_" + fileName, dll.ddlIterator());
-//	}
+	private static void QuickSort(DataIO io, String fileName) {
+		ArrayList<Integer> qc = new ArrayList<Integer>();
+		io.readDataSet(fileName, qc);
+		qc.sort_mergeSort();
+		io.writeDataSet("Quick_" + fileName, qc.arrIterator());
+	}
 }
